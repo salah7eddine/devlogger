6dfc92d2a52b2ef2a4eb35853812ff28472fa927
ABOUSAID <salah-eddine.abousaid@capgemini.com>
2019-07-06 18:25:10 +0100

    Observable & Component Communication

--- src/app/components/log-form/log-form.component.ts
+++ src/app/components/log-form/log-form.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Log } from './../../models/Log';
+import { LogService } from './../../services/log.service';
+
 @Component({
   selector: 'app-log-form',
   templateUrl: './log-form.component.html',
   styleUrls: ['./log-form.component.css']
 })
 export class LogFormComponent implements OnInit {
+  id: string;
+  text: string;
+  date: any;
 
-  constructor() { }
+  constructor(private logService: LogService) { }
 
   ngOnInit() {
+    // Subscribe to the selectedLog observable
+    this.logService.selectedLog.subscribe(log => {
+      if (log.id !== null) {
+        this.id = log.id;
+        this.text = log.text;
+        this.date = log.date;
+      }
+    });
   }
 
 }
